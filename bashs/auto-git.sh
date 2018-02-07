6409dc72bb376815dab5a3a01c597f09dadeaaa0
taoey <741494582@qq.com>
2018-02-07 20:53:14 +0800

    移除windows下的更新脚本

---
+++ bashs/auto-git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+path=docs
+time=`date +%Y%m%d-%H%M`
+cd $path
+
+echo "please input comment"
+read comment
+
+
+#for update
+git add .
+git commit -m "$time:$comment"
+git push -f -u origin
---
+++ bashs/io.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+readAndPrint(){
+  echo "please input something:"
+  read comment
+  echo your comment is: $comment
+}  
+###########
+# 主程序
+##########
+
+readAndPrint
+
+
