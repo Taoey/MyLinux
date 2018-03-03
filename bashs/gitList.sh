a350e089fb4697578e497c42ccc0ba29b7d6f243
taoey <741494582@qq.com>
2018-03-03 16:49:32 +0800

    20180303-1648:通过查询本地MongoDB数据库而制成的字典脚本

---
+++ bashs/gitList.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "
+My git-project List:
+1. wps-git
+    wps-git
+2. novel-git
+    n.r
+    n.n
+    n.g
+3.
+
+"
---
+++ bashs/py_dic.py
@@ -0,0 +1,29 @@
+import json
+from pymongo import MongoClient
+
+
+
+if __name__ == '__main__':
+    client = MongoClient('localhost', 27017)
+    #db name is dic
+    db = client.dic
+    #collection name is endic
+    endic = db.endic
+
+    while True:
+        word=input("Please input the word:")
+        # input "##",the script shutdown
+        if word == "##":
+            client.close()
+            print("The script shutdown")
+            break;
+        else:
+            rs=endic.find({"word_name":word},{"_id":0})#Attention: remember reomve "_id" column
+            for tmp in rs:
+                #word,explain,phonetic
+                part=tmp["symbols"][0]["parts"][0]["part"]
+                means=tmp["symbols"][0]["parts"][0]["means"]
+                ph_en=tmp["symbols"][0]["ph_en"]
+                ph_am=tmp["symbols"][0]["ph_am"]
+                ph_en_mp3=tmp["symbols"][0]["ph_en_mp3"]
+                print("[%s][%s]\n%s\n%s\n%s" % (ph_en,ph_am,part,means,ph_en_mp3))
